0446ac3dc497ef35f0d7e91f08cf40ba31cc419a
inji101 <injimamul@gmail.com>
2026-02-01 21:43:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ yesBtn.addEventListener("click", () => {
     result.innerHTML = "Tur murot hihot e xumai ase";
 });
 
+
 noBtn.addEventListener("mouseover", () => {
     const x = Math.random() * window.innerWidth * 0.6;
     const y = Math.random() * window.innerHeight * 0.6;
